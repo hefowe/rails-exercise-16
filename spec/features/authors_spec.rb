483aa189c7f1549f17510d55245b48a9b78569b3
Hendrik Folkerts <hendrik.folkerts@student.hpi.de>
2016-11-01 13:33:23 +0100

    Failing test for #2

--- spec/features/authors_spec.rb
+++ spec/features/authors_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe "Authors page", :type => :feature do
 
   it 'should render' do
-    visit "authors/new"
+    visit new_author_path
     expect(page).to have_text("New author")
   end
 end
