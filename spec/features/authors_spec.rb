042b6a6327608b434962d7309fefb56fcd766ccd
Hendrik Folkerts <hendrik.folkerts@student.hpi.de>
2016-11-01 13:56:58 +0100

    Failing test for #4

--- spec/features/authors_spec.rb
+++ spec/features/authors_spec.rb
@@ -11,4 +11,9 @@ describe "Authors page", :type => :feature do
     visit new_author_path
     expect(page).to have_field("first_name")
   end
+
+  it 'should have a "Create Author" button' do
+    visit new_author_path
+    expect(page).to have_button("Create Author")
+  end
 end
