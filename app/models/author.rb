667e51a7a039888d13411bba0b65a986534b91e3
Hendrik Folkerts <hendrik.folkerts@student.hpi.de>
2016-11-04 12:05:56 +0100

    Removed initialize method for Author. Closes #10.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,9 +1,3 @@
 class Author < ActiveRecord::Base
   attr_accessor :first_name, :last_name, :homepage
-
-  def initialize(first_name = "Alan", last_name = "Turing", homepage = "http://wikipedia.de/Alan_Turing")
-    @first_name = first_name
-    @last_name = last_name
-    @homepage = homepage
-  end
 end
