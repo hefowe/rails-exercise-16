82b7ae952a4dcc009cb9b5277bc4abe4b1c0e772
Hendrik Folkerts <hendrik.folkerts@student.hpi.de>
2016-11-04 12:14:11 +0100

    Failing test for #11.

--- spec/features/authors_class_spec.rb
+++ spec/features/authors_class_spec.rb
@@ -13,4 +13,14 @@ describe Author, :type => :feature do
     expect(author.last_name).to eq "Turing"
     expect(author.homepage).to eq "http://wikipedia.de/Alan_Turing"
   end
+
+  it 'should return the full name' do
+    author = Author.new
+
+    author.first_name = "Alan"
+    author.last_name = "Turing"
+    author.homepage = "http://wikipedia.de/Alan_Turing"
+
+    expect(author.name).to eq "Alan Turing"
+  end
 end
