564e02adfc5b2ce7687c8299001f00b4549e4757
Hendrik Folkerts <hendrik.folkerts@student.hpi.de>
2016-11-04 11:17:23 +0100

    Added initialize method to Author and attr_accessors. Closes #5. Closes #7. Closes #8.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,9 @@
 class Author < ActiveRecord::Base
+  attr_accessor :first_name, :last_name, :homepage
+
+  def initialize(first_name, last_name, homepage)
+    @first_name = first_name
+    @last_name = last_name
+    @homepage = homepage
+  end
 end
