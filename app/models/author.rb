5bd98ecd62db20330081fa2d6579c348d2f84bf5
Hendrik Folkerts <hendrik.folkerts@student.hpi.de>
2016-11-04 11:24:25 +0100

    Added default values for Author init. Closes #9.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,7 @@
 class Author < ActiveRecord::Base
   attr_accessor :first_name, :last_name, :homepage
 
-  def initialize(first_name, last_name, homepage)
+  def initialize(first_name = "Alan", last_name = "Turing", homepage = "http://wikipedia.de/Alan_Turing")
     @first_name = first_name
     @last_name = last_name
     @homepage = homepage
