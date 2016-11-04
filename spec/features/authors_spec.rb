c980d61af298433327659bf7f286197eb44453f6
Hendrik Folkerts <hendrik.folkerts@student.hpi.de>
2016-11-04 12:37:59 +0100

    Failing test for #12.

--- spec/features/authors_spec.rb
+++ spec/features/authors_spec.rb
@@ -18,4 +18,15 @@ describe "Authors page", :type => :feature do
     visit new_author_path
     expect(page).to have_button("Create Author")
   end
+
+  it 'should have a "Create Author" button' do
+    visit new_author_path
+
+    fill_in 'author_first_name', with: 'Alan'
+    fill_in 'author_last_name', with: 'Turing'
+    fill_in 'author_homepage', with: 'http://wikipedia.org/Alan_Turing'
+    submit_form
+
+    expect(Author.where(first_name: "Alan", last_name: "Turing")).to exist
+  end
 end
