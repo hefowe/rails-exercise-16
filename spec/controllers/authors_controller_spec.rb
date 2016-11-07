a46e83e3d7d9a06f67b0003b27664fc703418c8d
Hendrik Folkerts <hendrik.folkerts@student.hpi.de>
2016-11-07 22:03:16 +0100

    Refactored tests structure.

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -1,12 +1,102 @@
 require 'rails_helper'
 
-RSpec.describe AuthorsController, type: :controller do
+describe AuthorsController, :type => :feature do
 
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
+  it 'should render' do
+    visit new_author_path
+    expect(page).to have_text("New author")
+  end
+
+  it 'should have text input for first name, last name, and homepage' do
+    visit new_author_path
+    expect(page).to have_field("author_first_name")
+    expect(page).to have_field("author_last_name")
+    expect(page).to have_field("author_homepage")
+  end
+
+  it 'should have a "Create Author" button' do
+    visit new_author_path
+    expect(page).to have_button("Create Author")
+  end
+
+  it 'should save the author' do
+    visit new_author_path
+
+    fill_in 'author_first_name', with: 'Alan'
+    fill_in 'author_last_name', with: 'Turing'
+    fill_in 'author_homepage', with: 'http://wikipedia.org/Alan_Turing'
+    submit_form
+
+    expect(Author.where(first_name: "Alan", last_name: "Turing")).to exist
+  end
+
+  it 'should display author details' do
+    create(:author)
+
+    visit authors_path + "/1"
+
+    expect(page).to have_text("First name: Alan")
+    expect(page).to have_text("Last name: Turing")
+    expect(page).to have_text("Homepage: http://wikipedia.org/Alan_Turing")
+  end
+
+  it 'should render index route' do
+    visit authors_path
+    expect(page).to have_text("Listing authors")
+  end
+
+  it 'should link to the new author page on the index route' do
+    visit authors_path
+    expect(page).to have_css("a", :text => "Add author")
+  end
+
+  it 'should link to author page on the index route' do
+    create(:author)
+
+    visit authors_path
+    expect(page).to have_css("a", :text => "Show")
+  end
+
+  it 'should save the author' do
+    visit new_author_path
+
+    fill_in 'author_first_name', with: 'Alan'
+    fill_in 'author_last_name', with: ''
+    fill_in 'author_homepage', with: 'http://wikipedia.org/Alan_Turing'
+    submit_form
+
+    expect(page).to have_text("Last name can't be blank")
+  end
+
+  it 'should render edit route' do
+    create(:author)
+
+    visit edit_author_path(id: "1")
+
+    expect(page).to have_text("Editing author")
+  end
+
+  it 'should save changes on edit author page' do
+    create(:author)
+
+    visit edit_author_path(id: "1")
+
+    fill_in 'author_first_name', with: 'Alan Mathison'
+    submit_form
+  end
+
+  it 'should link to edit author page on the index route' do
+    create(:author)
+
+    visit authors_path
+    expect(page).to have_css("a", :text => "Edit")
+  end
+
+  it 'should have a link to delete an author on the index route' do
+    create(:author)
+
+    visit authors_path
+    expect(page).to have_css("a", :text => "Destroy")
   end
 
 end
--- spec/features/authors_spec.rb
+++ spec/features/authors_spec.rb
@@ -1,101 +1,32 @@
 require 'rails_helper'
 
-describe "Authors page", :type => :feature do
+describe Author, :type => :feature do
 
-  it 'should render' do
-    visit new_author_path
-    expect(page).to have_text("New author")
+  it 'should be created when initialized with values' do
+    author = Author.new
+
+    author.first_name = "Alan"
+    author.last_name = "Turing"
+    author.homepage = "http://wikipedia.de/Alan_Turing"
+
+    expect(author.first_name).to eq "Alan"
+    expect(author.last_name).to eq "Turing"
+    expect(author.homepage).to eq "http://wikipedia.de/Alan_Turing"
   end
 
-  it 'should have text input for first name, last name, and homepage' do
-    visit new_author_path
-    expect(page).to have_field("author_first_name")
-    expect(page).to have_field("author_last_name")
-    expect(page).to have_field("author_homepage")
+  it 'should return the full name' do
+    author = Author.new
+
+    author.first_name = "Alan"
+    author.last_name = "Turing"
+    author.homepage = "http://wikipedia.de/Alan_Turing"
+
+    expect(author.name).to eq "Alan Turing"
   end
 
-  it 'should have a "Create Author" button' do
-    visit new_author_path
-    expect(page).to have_button("Create Author")
-  end
-
-  it 'should save the author' do
-    visit new_author_path
-
-    fill_in 'author_first_name', with: 'Alan'
-    fill_in 'author_last_name', with: 'Turing'
-    fill_in 'author_homepage', with: 'http://wikipedia.org/Alan_Turing'
-    submit_form
-
-    expect(Author.where(first_name: "Alan", last_name: "Turing")).to exist
-  end
-
-  it 'should display author details' do
-    create(:author)
-
-    visit authors_path + "/1"
-
-    expect(page).to have_text("First name: Alan")
-    expect(page).to have_text("Last name: Turing")
-    expect(page).to have_text("Homepage: http://wikipedia.org/Alan_Turing")
-  end
-
-  it 'should render index route' do
-    visit authors_path
-    expect(page).to have_text("Listing authors")
-  end
-
-  it 'should link to the new author page on the index route' do
-    visit authors_path
-    expect(page).to have_css("a", :text => "Add author")
-  end
-
-  it 'should link to author page on the index route' do
-    create(:author)
-
-    visit authors_path
-    expect(page).to have_css("a", :text => "Show")
-  end
-
-  it 'should save the author' do
-    visit new_author_path
-
-    fill_in 'author_first_name', with: 'Alan'
-    fill_in 'author_last_name', with: ''
-    fill_in 'author_homepage', with: 'http://wikipedia.org/Alan_Turing'
-    submit_form
-
-    expect(page).to have_text("Last name can't be blank")
-  end
-
-  it 'should render edit route' do
-    create(:author)
-
-    visit edit_author_path(id: "1")
-
-    expect(page).to have_text("Editing author")
-  end
-
-  it 'should save changes on edit author page' do
-    create(:author)
-
-    visit edit_author_path(id: "1")
-
-    fill_in 'author_first_name', with: 'Alan Mathison'
-    submit_form
-  end
-
-  it 'should link to edit author page on the index route' do
-    create(:author)
-
-    visit authors_path
-    expect(page).to have_css("a", :text => "Edit")
-  end
-
-  it 'should have a link to delete an author on the index route' do
-    create(:author)
-
-    visit authors_path
-    expect(page).to have_css("a", :text => "Destroy")
+  it 'should not validate without last name' do
+    alan = create(:author)
+    alan.last_name = nil
+    expect(alan).to_not be_valid
   end
 end
