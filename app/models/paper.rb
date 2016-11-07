d66be2873d1ab8ca51b5d39f53d13383a1409094
Hendrik Folkerts <hendrik.folkerts@student.hpi.de>
2016-11-08 00:34:50 +0100

    Added getter for authors in papers. Clsoes #45.

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,10 +1,7 @@
 class Paper < ActiveRecord::Base
+  attr_accessor :authors
   validates :title, presence: true
   validates :venue, presence: true
   validates :year, presence: true, numericality: { only_integer: true }
-
-  def authors=(authors)
-
-  end
 
 end
