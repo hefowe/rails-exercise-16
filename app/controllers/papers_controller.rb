a92366e86597db9b9c6a33566273945707b0e3cc
Hendrik Folkerts <hendrik.folkerts@student.hpi.de>
2016-11-07 22:13:14 +0100

    Added paper model. Closes #29.

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,4 +1,16 @@
 class PapersController < ApplicationController
   def new
   end
+
+  def create
+    @paper = Paper.new(paper_params)
+
+    @paper.save
+    redirect_to @paper
+  end
+
+  private
+  def paper_params
+    params.require(:paper).permit(:title, :venue, :year)
+  end
 end
