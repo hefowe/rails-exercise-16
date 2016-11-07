5d2f41fc83c6f9cd41a56977774f256543976725
Hendrik Folkerts <hendrik.folkerts@student.hpi.de>
2016-11-07 21:51:58 +0100

    Failing test for #27.

--- spec/controllers/papers_controller_spec.rb
+++ spec/controllers/papers_controller_spec.rb
@@ -5,4 +5,11 @@ describe PapersController, :type => :feature do
     visit new_paper_path
     expect(page).to have_text("New paper")
   end
+
+  it 'should have text input for title, venue, and year' do
+    visit new_paper_path
+    expect(page).to have_field("paper_title")
+    expect(page).to have_field("paper_venue")
+    expect(page).to have_field("paper_year")
+  end
 end
