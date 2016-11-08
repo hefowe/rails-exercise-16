92a82e0888871216bb3fa7c1ae053c9a3e7f3da5
Hendrik Folkerts <hendrik.folkerts@student.hpi.de>
2016-11-08 13:34:43 +0100

    Modified paper_params. Closes #52.

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -27,7 +27,7 @@ class PapersController < ApplicationController
 
   def update
     @paper = Paper.find(params[:id])
-
+    logger.info "Processing the request..."
     if @paper.update(paper_params)
       redirect_to @paper
     else
@@ -44,6 +44,6 @@ class PapersController < ApplicationController
 
   private
   def paper_params
-    params.require(:paper).permit(:title, :venue, :year)
+    params.require(:paper).permit(:title, :venue, :year, :author_ids => [])
   end
 end
