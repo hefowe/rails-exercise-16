bab37df49d81251e89b8988777906b7185576918
Hendrik Folkerts <hendrik.folkerts@student.hpi.de>
2016-11-08 01:00:38 +0100

    Added acces for papers and init. Closes #48.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,8 +1,13 @@
 class Author < ActiveRecord::Base
+  attr_accessor :papers
   validates :last_name, presence: true
 
   def name
     first_name + ' ' + last_name
+  end
+
+  after_initialize do |author|
+    author.papers = []
   end
 
 end
