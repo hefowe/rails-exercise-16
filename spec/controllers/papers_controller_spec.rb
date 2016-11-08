51ace619b2d83c6a57492185bf0985efd843273f
Hendrik Folkerts <hendrik.folkerts@student.hpi.de>
2016-11-08 11:48:39 +0100

    Failing test for #50.

--- spec/controllers/papers_controller_spec.rb
+++ spec/controllers/papers_controller_spec.rb
@@ -97,4 +97,16 @@ describe PapersController, :type => :feature do
     expect(page).to have_text("Alan Turing")
   end
 
+  it 'should allow to select 5 authors from 5 separate drop downs' do
+    paper = create(:paper)
+
+    visit edit_paper_path(id: paper.id)
+
+    expect(page).to have_field("Author 1")
+    expect(page).to have_field("Author 2")
+    expect(page).to have_field("Author 3")
+    expect(page).to have_field("Author 4")
+    expect(page).to have_field("Author 5")
+  end
+
 end
